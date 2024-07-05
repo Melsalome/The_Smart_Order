c2597f052407dc7246f58b8911d83cdae076aa33
Hans Aparicio <hansaparicio@gmail.com>
2024-07-05 11:44:53 +0200

    render changes

--- src/api/models.py
+++ src/api/models.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
